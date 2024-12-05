576d254d9735cb2268d202bc3501a55f35641f57
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2024-12-05 16:27:14 -0500

    initial state

--- App.js
+++ App.js
@@ -1,10 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Brightness from 'expo-brightness';
+
+import {
+  initialize,
+  requestPermission,
+  readRecords,
+} from 'react-native-health-connect'
+
+import ReactNativeForegroundService from "@supersami/rn-foreground-service";
+ReactNativeForegroundService.register({
+  config: {
+  alert: true,
+  onServiceErrorCallBack: () => {
+  console.error("Foreground service error occurred");
+  },
+  }
+});
+
 
 export default function App() {
+  const update = async () => {
+    const isInitialized = await initialize();
+    if (!isInitialized) {
+      return;
+    }
+
+    const grantedPermissions = await requestPermission([
+      { accessType: 'read', recordType: 'Steps' },
+    ]);
+
+    const result = await readRecords('Steps', {
+      timeRangeFilter: {
+        operator: 'between',
+        startTime: String(new Date(new Date().setDate(new Date().getDate() - 1)).toISOString()),
+        endTime: String(new Date().toISOString())
+      },
+    });
+
+    console.log(result)
+
+    const { status } = await Brightness.requestPermissionsAsync();
+    if (status === 'granted') {
+      Brightness.setSystemBrightnessAsync(1);
+    }
+  }
+
+  update()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Walk some more to increase brightness!</Text>
       <StatusBar style="auto" />
     </View>
   );
